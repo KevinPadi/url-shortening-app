37879a7f7f5b7973c302879e66483f2b47a7e242
Kevin <kevpadilla01@gmail.com>
2023-09-11 23:54:21 -0300

    feat: added links item

--- src/components/InputSection.jsx
+++ src/components/InputSection.jsx
@@ -1,18 +1,36 @@
-import { useState } from 'react'
-import Button from './Button'
+import { useState, useEffect } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+import LinksItem from './LinksItem'
 
 function InputSection () {
   const [query, setQuery] = useState('')
   const [data, setData] = useState(null)
+  const [links, setLinks] = useState([])
 
   const handleClick = () => {
     query
       ? fetch(`https://api.shrtco.de/v2/shorten?url=${query}`)
         .then((res) => res.json())
-        .then((data) => setData(data.result.short_link))
-        .then(console.log(data))
+        .then((data) => setData(data))
       : console.log('no hay url')
   }
+
+  const onDelete = (id) => {
+    const newLinks = [...links]
+    const link = newLinks.find((link) => link.id === id)
+    const i = newLinks.indexOf(link)
+    newLinks.splice(i, 1)
+    setLinks(newLinks)
+  }
+
+  useEffect(() => {
+    if (data) {
+      const newData = data
+      const newLinks = [...links, { shortLink: newData.result.full_short_link, originalLink: newData.result.original_link, id: uuidv4() }]
+      setLinks(newLinks)
+      console.log(newLinks)
+    }
+  }, [data])
 
   return (
     <div className='w-full max-w-sm md:max-w-none mx-auto px-6 lg:px-20 xl:px-20'>
@@ -20,16 +38,11 @@ function InputSection () {
         <input value={query} onChange={e => { setQuery(e.target.value) }} type='text' className='w-full xl:w-[900px] text-sm md:text-base px-2 md:px-5 py-3 bg-white rounded-md focus:outline focus:outline-offset-0 focus:outline-2 focus:outline-GrayishViolet' placeholder='Shorten a link here...' />
         <button onClick={handleClick} className='py-3 bg-Cyan text-white font-bold hover:bg-Cyan/80 hover:transition-colors duration-100 rounded-md w-full md:w-40'>Shorten It!</button>
       </div>
-      <div className='flex flex-col gap-3'>
-        <div className='text-left md:px-5 py-3 flex flex-col md:flex-row justify-between md:items-center bg-white rounded-md divide-y-[0.5px] md:divide-none divide-Gray/40'>
-          <div className='px-3 pb-3 md:px-0 md:pb-0'>
-            <p className='text-VeryDarkBlue text-sm md:text-base lg:text-lg'>https://www.frontendmentor.io</p>
-          </div>
-          <div className='px-3 pt-3 md:px-0 md:pt-0 flex flex-col md:flex-row text-center md:justify-between md:items-center gap-2 md:gap-5 w-full md:w-auto'>
-            <p className='text-Cyan text-sm md:text-base lg:text-lg text-left'>shrtco.de/4M7SX9</p>
-            <Button label='Copy' rounded='rounded-md' width='px-6 py-2' />
-          </div>
-        </div>
+      <div className='flex flex-col gap-3 max-h-[320px] md:max-h-60 overflow-y-scroll'>
+        {data
+          ? links.map((links) => (<LinksItem links={links} onDelete={onDelete} key={links.id} />))
+          : null}
+
       </div>
     </div>
   )
