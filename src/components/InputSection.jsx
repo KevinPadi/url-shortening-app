b08c6abea3fd0fa8463acf1aef09cc02de119478
Kevin <kevpadilla01@gmail.com>
2023-09-12 23:35:21 -0300

    feat: input styles when is invalid

--- src/components/InputSection.jsx
+++ src/components/InputSection.jsx
@@ -6,15 +6,12 @@ function InputSection () {
   const [query, setQuery] = useState('')
   const [data, setData] = useState(null)
   const [links, setLinks] = useState([])
+  const [isSubmitted, setIsSubmitted] = useState(false)
 
-  const handleClick = (e) => {
-    e.preventDefault()
-    query
-      ? fetch(`https://api.shrtco.de/v2/shorten?url=${query}`)
-        .then((res) => res.json())
-        .then((data) => setData(data))
-      : console.log('no hay url')
-  }
+  // const handleClick = (e) => {
+  //   e.preventDefault()
+
+  // }
 
   const onDelete = (id) => {
     const newLinks = [...links]
@@ -33,17 +30,39 @@ function InputSection () {
     }
   }, [data])
 
+  // TODO: mirar chatgpt para seguir con lo del input, manejar error al mandar una url incorrecta a la api
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if (query.trim() === '') {
+      setIsSubmitted(true)
+    } else {
+      fetch(`https://api.shrtco.de/v2/shorten?url=${query}`)
+        .then((res) => res.json())
+        .then((data) => setData(data))
+      setIsSubmitted(false)
+    }
+  }
+
+  const inputClass = `w-full text-sm md:text-base px-2 md:px-5 py-3 bg-white rounded-md focus:outline focus:outline-offset-0 focus:outline-2 focus:outline-Cyan ${isSubmitted && query.trim() === '' ? 'outline outline-Red focus:outline-Red placeholder:text-Red/50' : 'placeholder:text-Gray'}`
+
   return (
     <div className='w-full max-w-sm md:max-w-none mx-auto px-6 lg:px-20 xl:px-20'>
-      <form className='flex flex-col md:flex-row gap-4 lg:gap-8 justify-between items-center p-5 md:p-6 lg:p-10 xl:h-32 my-10 bg-DarkViolet bg-shortenMobileBg md:bg-shortenDeskBg bg-no-repeat bg-contain md:bg-cover bg-right-top md:bg-top rounded-lg'>
-        <input value={query} onChange={e => { setQuery(e.target.value) }} type='text' className='w-full xl:w-[900px] text-sm md:text-base px-2 md:px-5 py-3 bg-white rounded-md focus:outline focus:outline-offset-0 focus:outline-2 focus:outline-GrayishViolet' placeholder='Shorten a link here...' />
-        <button onClick={handleClick} className='py-3 bg-Cyan text-white font-bold hover:bg-Cyan/80 hover:transition-colors duration-100 rounded-md w-full md:w-40'>Shorten It!</button>
+      <form onSubmit={handleSubmit} className='flex flex-col md:flex-row gap-4 lg:gap-8 justify-between items-center p-5 md:p-6 lg:p-10 xl:h-32 my-10 bg-DarkViolet bg-shortenMobileBg md:bg-shortenDeskBg bg-no-repeat bg-contain md:bg-cover bg-right-top md:bg-top rounded-lg'>
+        <div className='w-full xl:w-[900px] flex flex-col gap-2 justify-center'>
+          <input value={query} onChange={e => { setQuery(e.target.value) }} type='text' className={inputClass} placeholder='Shorten a link here...' />
+          {isSubmitted && query.trim() === '' && (
+            <p className='text-Red text-xs block md:absolute italic md:top-[80px] lg:top-[100px]'>Please add a link</p>
+          )}
+
+        </div>
+        <button type='submit' className='py-3 bg-Cyan text-white font-bold hover:bg-Cyan/80 hover:transition-colors duration-100 rounded-md w-full md:w-40'>Shorten It!</button>
       </form>
       <div className='flex flex-col gap-3 max-h-[320px] md:max-h-60 overflow-y-scroll'>
         {data
           ? links.map((links) => (<LinksItem links={links} onDelete={onDelete} key={links.id} />))
           : null}
-
       </div>
     </div>
   )
